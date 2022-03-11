022059c22b3ad3d8538db457508dcb5687bbff7e
chg1f <chongiofai@gmail.com>
2022-03-11 15:38:23 +0800

    reflactor: resolve

--- resolve.py
+++ resolve.py
@@ -23,6 +23,28 @@ def _merge(hosts: typing.List[str], paths: typing.List[str]) -> typing.List[str]
                 results.add(line.strip())
     return list(results)
 
+
+async def _query(resolver: aiodns.DNSResolver, host: str, qtype: str):
+    result = {"query": host, "query_type": qtype}
+    try:
+        temp = await resolver.query(host=host, qtype=qtype)
+        if not isinstance(temp, typing.Iterable):
+            temp = [temp]
+        for t in temp:
+            for k in t.__slots__:
+                if k not in result:
+                    result[k] = list() # type: ignore
+                result[k].append(getattr(t, k)) # type: ignore
+    except asyncio.CancelledError:
+        raise
+    except aiodns.error.DNSError as e:
+        logging.error("{} query {}: {!r}".format(host, qtype, e))
+    except Exception as e:
+        logging.exception("{} query {}: {!r}".format(host, qtype, e))
+        raise
+    return result
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -45,52 +67,35 @@ if __name__ == "__main__":
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
-
-    fp = None
-    if args.output != "stdout":
-        fp = open(args.output, "a")
-
     hosts = _merge(hosts=args.host, paths=args.hosts)
     nameservers = _merge(hosts=args.nameserver, paths=args.nameservers)
     qtypes = list(set(t for t in args.qtypes if t in aiodns.query_type_map.keys()))
-    loop = asyncio.get_event_loop()
-
     assert all(
         [t in aiodns.query_type_map.keys() for t in qtypes]
     ), "unexpect resolve query type"
+
+    output = None
+    if args.output != "stdout":
+        output = open(args.output, "a")
+
     loop = asyncio.get_event_loop()
     resolver = aiodns.DNSResolver(nameservers=nameservers, loop=loop)
 
-    temp = dict()
+    tasks = []
     for host in hosts:
-        temp[host] = []
         for qtype in qtypes:
-            temp[host].append(resolver.query(host=host, qtype=qtype))
-    for host, futures in temp.items():
-        try:
-            loop.run_until_complete(asyncio.gather(*futures, loop=loop))
-        except Exception as e:
-            logging.error("{}: {!r}".format(host, e))
-    loop.close()
-
-    results: typing.List[typing.Dict] = []
-    for host, futures in temp.items():
-        for future in futures:
-            if not future.done():
-                continue
-            try:
-                result = future.result()
-            except Exception as e:
-                logging.error("{}: {!r}".format(host, e))
-                continue
-
-            if not isinstance(result, typing.Iterable):
-                result = [result]
-            for r in result:
-                t = {"query": host}
-                for k in r.__slots__:
-                    t[k] = getattr(r, k)
-                if fp:
-                    fp.write(json.dumps(t)+"\n")
-                # results.append(t)
-                print(t)
+            tasks.append(
+                loop.create_task(_query(resolver=resolver, host=host, qtype=qtype))
+            )
+    try:
+        loop.run_until_complete(asyncio.gather(*tasks))
+    finally:
+        loop.close()
+    for t in tasks:
+        if not t.done():
+            continue
+        line = json.dumps(t.result())
+        if output:
+            output.write(line + "\n")
+        else:
+            print(line)
